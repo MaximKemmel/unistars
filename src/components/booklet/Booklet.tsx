64e4064448afb2c94351c1248b41831513a4118a
MaximKemmel <kemmel7007@gmail.com>
2025-01-07 08:27:51 +0200

    Commit

--- src/components/booklet/Booklet.tsx
+++ src/components/booklet/Booklet.tsx
@@ -9,7 +9,7 @@ interface IBookletProps {
 export const Booklet: React.FC<IBookletProps> = ({ bookletItem }) => {
   return (
     <div className={styles.booklet_container}>
-      <img src={bookletItem.photo} alt="" />
+      <img src={bookletItem.photo}  alt="" />
       <div className={styles.booklet_info}>
         <div className={styles.booklet_name}>{bookletItem.name}</div>
         <div className={styles.booklet_description}>
