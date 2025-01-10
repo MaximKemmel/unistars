c2771d1444c3859c5c80438757150c0b4435479a
MaximKemmel <kemmel7007@gmail.com>
2025-01-10 19:24:00 +0200

    https

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const instance = axios.create();
 
 instance.interceptors.request.use((config) => {
-    config.headers.Authorization = window.localStorage.getItem("sheriff_md_token");
-    config.headers.Accept = { 'Access-Control-Allow-Credentials': true };
+    //config.headers.Authorization = window.localStorage.getItem("sheriff_md_token");
     return config;
 });
 
