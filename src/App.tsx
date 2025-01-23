498e5a6af3e8569b490c6017dc67cea6d4e0251f
MaximKemmel <52887742+MaximKemmel@users.noreply.github.com>
2025-01-24 00:21:52 +0200

    Правки

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import {useEffect, useLayoutEffect} from "react";
+import { useLayoutEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { settings } from '@gravity-ui/date-utils';
-import {configure, ThemeProvider} from '@gravity-ui/uikit';
-import { Lang } from '@gravity-ui/uikit';
 
 import { useActions } from "./hooks/useActions";
 
@@ -13,19 +9,12 @@ import { SignIn } from "./pages/SignIn/SignIn";
 import styles from "./App.module.sass";
 import { useTypedSelector } from "./hooks/useTypedSelector";
 
-await settings.loadLocale('ru');
-
 function App() {
-  const { i18n } = useTranslation();
   const { getCountries, getCities } = useActions();
   const countries = useTypedSelector((state) => state.coreReducer.countries);
   const cities = useTypedSelector((state) => state.coreReducer.cities);
 
   useLayoutEffect(() => {
-    if (i18n.resolvedLanguage != null) {
-      settings.setLocale(i18n.resolvedLanguage);
-      configure({ lang: i18n.resolvedLanguage as Lang });
-    }
     if (!Array.isArray(countries) || countries.length === 0) {
       getCountries();
     }
@@ -34,22 +23,13 @@ function App() {
     }
   }, []);
 
-  useEffect(() => {
-    if (i18n.resolvedLanguage != null) {
-      settings.setLocale(i18n.resolvedLanguage);
-      configure({ lang: i18n.resolvedLanguage as Lang });
-    }
-  }, [i18n.resolvedLanguage]);
-
   return (
-    <ThemeProvider theme="light">
-      <section className={styles.wrapper}>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </section>
-    </ThemeProvider>
+    <section className={styles.wrapper}>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </section>
   );
 }
 
