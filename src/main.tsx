22e902c5170f03f674782904adee7f0de2d38b75
MaximKemmel <52887742+MaximKemmel@users.noreply.github.com>
2025-02-19 14:17:52 +0200

    Правки

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { createRoot } from "react-dom/client";
-import { Await, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { settings } from "@gravity-ui/date-utils";
 
@@ -13,15 +13,13 @@ import "./utils/i18n.js";
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
-async function initData() {
-  await settings.loadLocale("ru");
-}
+await settings.loadLocale("ru");
 
 root.render(
   <BrowserRouter>
     <Provider store={store}>
       <Suspense fallback="...loading">
-        <Await resolve={initData} children={<App />} />
+        <App />
       </Suspense>
     </Provider>
   </BrowserRouter>,
--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,18 +1,23 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend"
+import Backend from "i18next-http-backend";
 
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
     debug: true,
     fallbackLng: "ru",
     detection: {
-        order: ['queryString', 'cookie'],
-        cache: ['cookie']
+      order: ["queryString", "cookie"],
+      cache: ["cookie"],
     },
     interpolation: {
-        escapeValue: false
-    }
-})
+      escapeValue: false,
+    },
+  })
+  .then();
 
 export default i18n;
