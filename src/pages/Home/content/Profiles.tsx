3eded847980d4d743fb9e266d6aea597459ff252
MaximKemmel <52887742+MaximKemmel@users.noreply.github.com>
2025-03-27 12:12:01 +0200

    Профили

--- src/pages/Home/content/Profiles.tsx
+++ src/pages/Home/content/Profiles.tsx
@@ -1,26 +1,35 @@
 import { useEffect, useState } from "react";
 
-import { ProfilesFiltersModal } from "../../../modals/ProfilesFilters/ProfilesFilters";
+import { useTypedSelector } from "../../../hooks/useTypedSelector";
 
-import globalStyles from "../../../App.module.sass";
-import styles from "../Home.module.sass";
+import { ProfilesFiltersModal } from "../../../modals/ProfilesFilters/ProfilesFilters";
 
 import { Input } from "../../../components/input/Input";
 import { Toggle } from "../../../components/toggle/Toggle";
 
+import globalStyles from "../../../App.module.sass";
+import styles from "../Home.module.sass";
+
 import { IToggleItem } from "../../../types/local/toggleItem";
 import { initUserFilter } from "../../../types/userFilter/initUserFilter";
+import { IUser } from "../../../types/user/user";
 
 import { Filter as FilterIcon } from "../../../assets/svgComponents/Filter";
+import EmptyAvatarImage from "../../../assets/png/empty-avatar.png";
 
 export const Profiles = () => {
+  const users = useTypedSelector(
+    (state) => state.subscriberReducer.subscriberList,
+  );
   const [viewMode, setViewMode] = useState(0);
   const [filter, setFilter] = useState(initUserFilter());
   const [searchValue, setSearchValue] = useState("");
   const [isFiltersModalShow, setIsFiltersModalShow] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     setFilter(initUserFilter());
+    setCurrentPage(1);
   }, [viewMode]);
 
   return (
@@ -73,6 +82,155 @@ export const Profiles = () => {
               <FilterIcon isActive={true} />
             </button>
           </div>
+        </div>
+        <div
+          className={`${styles.content_container} ${styles.profiles_table_container}`}
+        >
+          {viewMode === 0 ? (
+            <div className={styles.profiles_table}>
+              <div className={styles.table_head}>
+                <div className={styles.th} style={{ width: "20%" }}>
+                  Имя Фамилия
+                </div>
+                <div className={styles.th} style={{ width: "20%" }}>
+                  Гражданство
+                </div>
+                <div className={styles.th} style={{ width: "17%" }}>
+                  Жел.образование
+                </div>
+                <div className={styles.th} style={{ width: "15%" }}>
+                  Жел.направление
+                </div>
+                <div className={styles.th} style={{ width: "13%" }}>
+                  Год поступления
+                </div>
+                <div className={styles.th} style={{ width: "13%" }}>
+                  Язык обучения
+                </div>
+              </div>
+              <div className={styles.table_body}>
+                {users
+                  .slice((currentPage - 1) * 10, currentPage * 10)
+                  .map((user: IUser) => (
+                    <div className={styles.table_row}>
+                      <div
+                        className={`${styles.tr} ${styles.name}`}
+                        style={{ width: "20%" }}
+                      >
+                        <div className={styles.avatar}>
+                          {user.avatarUrl ? (
+                            <img src={user.avatarUrl} alt="" />
+                          ) : (
+                            <img src={EmptyAvatarImage} alt="" />
+                          )}
+                        </div>
+                        {user.fullName}
+                      </div>
+                      <div className={styles.tr} style={{ width: "20%" }}>
+                        Казахстан, Узбекистан
+                      </div>
+                      <div className={styles.th} style={{ width: "17%" }}>
+                        Бакалавриат
+                      </div>
+                      <div className={styles.th} style={{ width: "15%" }}>
+                        Гуманитарные и социальные науки
+                      </div>
+                      <div className={styles.th} style={{ width: "13%" }}>
+                        2025
+                      </div>
+                      <div className={styles.th} style={{ width: "13%" }}>
+                        {user.languages !== null &&
+                        user.languages !== undefined &&
+                        user.languages.length > 0
+                          ? user.languages[0].name
+                          : ""}
+                      </div>
+                    </div>
+                  ))}
+              </div>
+              {users.length > 10 ? (
+                <div className={styles.pagination}>
+                  {Array(Math.ceil(users.length / 10))
+                    .fill(1)
+                    .map((_, index: number) => (
+                      <div onClick={() => setCurrentPage(index + 1)}>
+                        {index + 1}
+                      </div>
+                    ))}
+                </div>
+              ) : null}
+            </div>
+          ) : (
+            <div className={styles.profiles_table}>
+              <div className={styles.table_head}>
+                <div className={styles.th} style={{ width: "20%" }}>
+                  Имя Фамилия
+                </div>
+                <div className={styles.th} style={{ width: "20%" }}>
+                  Гражданство
+                </div>
+                <div className={styles.th} style={{ width: "17%" }}>
+                  Университет
+                </div>
+                <div className={styles.th} style={{ width: "15%" }}>
+                  Образование
+                </div>
+                <div className={styles.th} style={{ width: "13%" }}>
+                  Направление
+                </div>
+                <div className={styles.th} style={{ width: "13%" }}>
+                  Год окончания
+                </div>
+              </div>
+              <div className={styles.table_body}>
+                {users
+                  .slice((currentPage - 1) * 10, currentPage * 10)
+                  .map((user: IUser) => (
+                    <div className={styles.table_row}>
+                      <div
+                        className={`${styles.tr} ${styles.name}`}
+                        style={{ width: "20%" }}
+                      >
+                        <div className={styles.avatar}>
+                          {user.avatarUrl ? (
+                            <img src={user.avatarUrl} alt="" />
+                          ) : (
+                            <img src={EmptyAvatarImage} alt="" />
+                          )}
+                        </div>
+                        {user.fullName}
+                      </div>
+                      <div className={styles.tr} style={{ width: "20%" }}>
+                        Казахстан, Узбекистан
+                      </div>
+                      <div className={styles.th} style={{ width: "17%" }}>
+                        СФУ
+                      </div>
+                      <div className={styles.th} style={{ width: "15%" }}>
+                        Бакалавриат
+                      </div>
+                      <div className={styles.th} style={{ width: "13%" }}>
+                        Гуманитарные и социальные науки
+                      </div>
+                      <div className={styles.th} style={{ width: "13%" }}>
+                        2025
+                      </div>
+                    </div>
+                  ))}
+              </div>
+              {users.length > 10 ? (
+                <div className={styles.pagination}>
+                  {Array(Math.ceil(users.length / 10))
+                    .fill(1)
+                    .map((_, index: number) => (
+                      <div onClick={() => setCurrentPage(index + 1)}>
+                        {index + 1}
+                      </div>
+                    ))}
+                </div>
+              ) : null}
+            </div>
+          )}
         </div>
       </div>
       <ProfilesFiltersModal
